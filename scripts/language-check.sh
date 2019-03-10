5ab7465a6d616dbae38aa7a94efb1b84a13d5932
Chris Manson <chris@manson.ie>
2019-03-10 19:52:03 +0000

    fixing alex build

--- scripts/language-check.sh
+++ scripts/language-check.sh
@@ -1,7 +1,7 @@
-git remote add base https://github.com/emberjs/website.git
+git remote add base https://github.com/ember-learn/ember-blog.git
 git fetch base
-BLOG_HAS_CHANGES=$(git diff --name-status base/master source/blog/2*)
+BLOG_HAS_CHANGES=$(git diff --name-status base/master source/2*)
 if [[ $BLOG_HAS_CHANGES ]]
   then
-    alex $(git diff --name-status base/master source/blog/2* | sed s/^..//)
+    alex $(git diff --name-status base/master source/2* | sed s/^..//)
 fi
