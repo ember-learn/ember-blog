8100d17fdba4e5c05499ad7537d03544f192cf3a
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-06-13 16:46:07 +0200

    blog(layouts): add test ids for automatic goodbitsing on embertimes posts

---
+++ source/javascripts/embertimes-test-ids.js
@@ -0,0 +1,27 @@
+$(function(){
+  if ($('[data-embertimes-page]').length > 0) {
+    applyEmberTimesTestIds();
+  }
+
+
+   function applyEmberTimesTestIds() {
+     let sectionTitles = $('[data-embertimes-post-body] h2');
+     sectionTitles.attr('data-embertimes-section-title', true);
+
+     sectionTitles.each((i, title) => {
+       $(title).attr('data-embertimes-section-title', i);
+       let sectionFragments = $(title).nextUntil('h2').not('hr');
+       sectionFragments.each((index, element) => {
+         $(element).attr('data-embertimes-section-fragment', i);
+       });
+     });
+
+     let introFragmentFirst = $('[data-embertimes-post-body]').children().first();
+     introFragmentFirst.attr('data-embertimes-intro-fragment', true);
+     let introFragments = introFragmentFirst.nextUntil('h2').not('hr');
+
+     introFragments.each((i, el) => {
+       $(el).attr('data-embertimes-intro-fragment', true);
+     });
+   }
+ });
