528f015518504d33b53a5e73817319af605d3caf
Alexey Kulakov <uni_que@me.com>
2019-07-31 17:06:52 -0700

    adding markdown lint with default config

---
+++ scripts/markdown-lint.sh
@@ -0,0 +1,7 @@
+git remote add base https://github.com/ember-learn/ember-blog.git
+git fetch base
+BLOG_HAS_CHANGES=$(git diff --name-status base/master source/2*)
+if [[ $BLOG_HAS_CHANGES ]]
+  then
+    markdownlint $(git diff --name-status base/master source/2* | sed s/^..//*.md)
+fi
