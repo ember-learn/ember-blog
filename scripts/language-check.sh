fb2df5085d20bac336568296472ee9a23dc8d7c4
Alexey Kulakov <uni_que@me.com>
2019-08-02 13:32:27 -0700

    fixed globing patter to lint all markdown file in source folder

--- scripts/language-check.sh
+++ scripts/language-check.sh
@@ -3,5 +3,5 @@ git fetch base
 BLOG_HAS_CHANGES=$(git diff --name-status base/master source/2*)
 if [[ $BLOG_HAS_CHANGES ]]
   then
-    alex $(git diff --name-status base/master source/2* | sed s/^..//)
+    alex $(git diff --name-status base/master source/*.md | sed s/^..//)
 fi
