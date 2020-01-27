ff34cef6d170f5f6881d1f23ede1eca9e84b1f24
Eli Flanagan <efx@users.noreply.github.com>
2020-01-27 11:38:28 -0500

    condense validation behavior
    Both scripts differed only by the binary name so it makes sense to
    condense them into one. I also specified the interpreter using a shebang for
    flexibility.

--- scripts/markdown-lint.sh
+++
@@ -1,7 +0,0 @@
-git remote add base https://github.com/ember-learn/ember-blog.git
-git fetch base
-BLOG_HAS_CHANGES=$(git diff --name-status base/master source/2*)
-if [[ $BLOG_HAS_CHANGES ]]
-  then
-    markdownlint $(git diff --name-status base/master source/*.md | sed s/^..//)
-fi
---
+++ scripts/validate.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2086
+linting_binary=$1
+
+if [ -z "$linting_binary" ]; then
+	echo "Please specify a linting binary."
+	exit 1
+fi
+git remote add base https://github.com/ember-learn/ember-blog.git
+git fetch base
+BLOG_HAS_CHANGES=$(git diff --name-status base/master source/2*)
+if [[ $BLOG_HAS_CHANGES ]]
+  then
+    $linting_binary $(git diff --name-status base/master source/*.md | sed s/^..//)
+fi
