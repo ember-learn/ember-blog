3c5eb27742bd97d4134087d768b366dbe839b13c
Chris Manson <chris@manson.ie>
2020-11-26 14:28:13 +0000

    use the page route for pagination instead of "pages"

---
+++ app/routes/page.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+
+export default class PageRoute extends Route {
+  templateName = 'index';
+
+  model(params) {
+    return this.store.query('content', {page: `/content/content-${params.id}.json`});
+  }
+}
