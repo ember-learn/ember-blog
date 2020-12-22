7af720fa28c23f551726945844574cf04bc4c15e
ijlee2 <ijlee2@users.noreply.github.com>
2020-12-22 10:10:38 -0600

    Configured ember-prism to support more languages

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,23 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    // Theme options are coy, dark, funky, okaidia, solarizedlight, tomorrow,
+    // and twilight. Remove the `theme` attribute to use the default theme.
+    'ember-prism': {
+      theme: 'tomorrow',
+      components: [
+        'bash',
+        'handlebars', 'markup-templating', // handlebars
+        'javascript',
+        'json',
+        'python',
+        'typescript',
+      ],
+      plugins: [
+        'line-numbers',
+      ],
+    },
+
     fingerprint: {
       extensions: ['js', 'css', 'map']
     }
